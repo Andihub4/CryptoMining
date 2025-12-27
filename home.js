3f970c3f4bd326b26d75e730bcb44f8dd8e1573a
andihub4 <andinetatx@gmail.com>
2025-12-27 09:01:57 +0300

    update

--- home.js
+++ home.js
@@ -1,43 +1,31 @@
 import React from 'react';
+
+
+
+
 class Home extends React.Component{
   render() {
   return (
     //home
     <div>
+      
+      
       <span style={{backgroundColor: 'rgb(6, 13, 20)'}}>
-            <span style={{ color: 'white', borderColor :'black', marginLeft: '10px', borderRadius: '10px', backgroundColor: 'rgb(6, 13, 31)', borderColor: 'green',paddingLeft: '20px'}} >100</span>
-      <span style={{ color: 'green',
-      borderColor :'black', marginLeft: '85px', borderRadius: '10px',paddingLeft: '20px',borderColor: 'green',backgroundColor: 'rgb(6, 13, 31)'
-      }} >$100.0</span>
+            <button style={{ color: 'white', borderColor :'rgba(57, 255, 20, 1)', marginLeft: '10px', borderRadius: '10px', backgroundColor: 'rgb(6, 13, 31)',paddingLeft: '20px'}} >100</button>
+            
+      <button style={{ color: 'green',
+      borderColor :'rgba(57, 255, 20, 1)', marginLeft: '70px', borderRadius: '10px',paddingLeft: '20px',backgroundColor: 'rgb(6, 13, 31)'
+      }} >$16.5</button>
       
       
-      <span style={{ color: 'red', borderColor :'black', marginLeft: '100px', borderRadius: '17px'}} data-bs-toggle="offcanvas" data-bs-target="#offcanvasResponsive" aria-controls="offcanvasResponsive">👤</span> </span>
+      <button style={{ color: 'rgba(57, 255, 20, 1)', borderColor :'rgba(57, 255, 20, 1)', marginLeft: '80px', borderRadius: '17px', backgroundColor: 'rgb(6, 13, 31)'}}>
+
+<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
+</svg>
+</button> </span>
 
 
-
-
-
-<div class="alert alert-info d-none d-lg-block">Resize your browser to show the responsive offcanvas toggle.</div>
-
-<div style={{backgroundColor: 'rgb(6, 13, 31)'}} class="offcanvas-lg offcanvas-end" tabindex="-1" id="offcanvasResponsive" aria-labelledby="offcanvasResponsiveLabel">
-  <div style={{backgroundColor: 'rgb(6, 13, 31)'}} class="offcanvas-header">
-    <h5 style={{backgroundColor: 'rgb(6, 13, 31)'}} class="offcanvas-title" id="offcanvasResponsiveLabel">Responsive offcanvas</h5>
-    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#offcanvasResponsive" aria-label="Close"></button>
-  </div>
-  <div class="offcanvas-body">
-    <p style={{backgroundColor: 'rgb(6, 13, 31)'}} class="mb-0">
-       
-    <li><a class="dropdown-item" href="#">Profile</a></li>
-    <li><a class="dropdown-item" href="#">Withdraw</a></li>
-        <li><a class="dropdown-item" href="#">A2A</a></li>
-    <li><a class="dropdown-item" href="#">Settings</a></li>
-        <li><a class="dropdown-item" href="#">about</a></li>
-            <li><a class="dropdown-item" href="#">help</a></li>
-  
-    
-    </p>
-  </div>
-</div>
 
 
 
@@ -51,35 +39,151 @@ class Home extends React.Component{
 
       
 <hr style = {{color:"white"}}></hr>
-          
-       <button style={{borderRadius: '16px', marginLeft: '160px'}}><img src="images.png" style={{width: '20px'}}></img></button>
-       
-       <p>.</p>
-       <p>.</p>
-       
-       <a style={{borderRadius: '16px', marginLeft: '150px'}} href="#" class="btn btn-danger">Start</a>
-       <p>.</p>
-       <p>.</p>
-      
     
-      
-       
-       
-      
-       
-       
-       
-       <div style={{ backgroundColor: 'rgb(6, 13, 20)',paddingTop: '20px'}} class="card">
-  <div style = {{backgroundColor: 'rgb(6, 13, 20)'}} class="card-header">
-    Mining History
+    
+    
+      <div style={{ backgroundColor: 'rgb(6, 13, 20)',paddingTop: '20px', borderColor: 'rgba(57, 255, 20, 1)'}} class="card">
+  <div style = {{backgroundColor: 'rgb(6, 13, 20)', borderColor: 'black',color: 'white'}} class="card-header">
+    
+    <span style={{fontSize: '20px', marginLeft: '10px'}}>Arsenal</span> 
+    <span style={{fontSize: '30px', color: "rgba(57, 255, 20, 1)", marginLeft: '50px'}}>Vs</span> 
+    
+    <span style={{fontSize: '20px', marginLeft: '40px'}}>City</span>
+    
+    
   </div>
   <div class="card-body">
     
-    <h5 style={{backgroundColor: 'rgb(6, 13, 31)'}} class="card-title">Special title treatment</h5>
-    <p style={{backgroundColor : 'rgb(6, 13, 31)'}} class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <h5 style={{backgroundColor: 'rgb(6, 13, 31)', borderColor: 'black', color: 'white'}} class="card-title">
+      
+      0×00000000000000000000
+      <span>
+        <button>
+          <img>
+          </img>
+          </button>
+          </span>
+      Vs
+      
+      <span>
+        <button>
+          <img>
+          </img>
+          </button>
+          </span>
+      
+      </h5>
+    <p style={{backgroundColor : 'rgb(6, 13, 31)', borderColor: 'rgba(57, 255, 20, 1)'}} class="card-text"></p>
     
   </div>
-</div>   
+</div>
+          
+
+    
+    
+    
+
+    
+    
+
+    
+       
+       
+
+       
+
+      <p>.</p>
+      <p>.</p>
+      <p>.</p>
+      
+       
+       
+      
+       
+       
+      
+       
+      
+     
+     
+    
+
+       
+  
+<hr style={{color: 'white',}}></hr>
+  
+<svg style={{marginLeft: '15px', color: 'rgba(57, 255, 20, 1)'}} xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+  <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z"/>
+</svg>
+
+
+
+
+
+
+
+<svg style={{marginLeft: '30px', color: 'rgba(57, 255, 20, 1)'}} xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-record-circle" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+  <path d="M11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+</svg>
+
+
+
+
+
+
+
+
+  
+  
+  
+<svg style={{marginLeft: '30px', color: 'rgba(57, 255, 20, 1)'}} xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-speedometer" viewBox="0 0 16 16">
+  <path d="M8 2a.5.5 0 0 1 .5.5V4a.5.5 0 0 1-1 0V2.5A.5.5 0 0 1 8 2M3.732 3.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707M2 8a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 8m9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5m.754-4.246a.39.39 0 0 0-.527-.02L7.547 7.31A.91.91 0 1 0 8.85 8.569l3.434-4.297a.39.39 0 0 0-.029-.518z"/>
+  <path fill-rule="evenodd" d="M6.664 15.889A8 8 0 1 1 9.336.11a8 8 0 0 1-2.672 15.78zm-4.665-4.283A11.95 11.95 0 0 1 8 10c2.186 0 4.236.585 6.001 1.606a7 7 0 1 0-12.002 0"/>
+</svg>  
+  
+  
+  
+  
+  
+
+
+
+
+
+
+<svg style={{marginLeft: '29px', color: 'rgba(57, 255, 20, 1)'}} xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-anthropic" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M9.218 2h2.402L16 12.987h-2.402zM4.379 2h2.512l4.38 10.987H8.82l-.895-2.308h-4.58l-.896 2.307H0L4.38 2.001zm2.755 6.64L5.635 4.777 4.137 8.64z"/>
+</svg>
+
+
+
+
+
+
+
+
+
+<svg style={{marginLeft: '35px', color: 'rgba(57, 255, 20, 1)'}} xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-wallet2" viewBox="0 0 16 16">
+  <path d="M12.136.326A1.5 1.5 0 0 1 14 1.78V3h.5A1.5 1.5 0 0 1 16 4.5v9a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 13.5v-9a1.5 1.5 0 0 1 1.432-1.499zM5.562 3H13V1.78a.5.5 0 0 0-.621-.484zM1.5 4a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5z"/>
+</svg>
+
+     
+      
+       
+      
+
+
+
+<svg style={{marginLeft: '40px', color: 'rgba(57, 255, 20, 1)'}} xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16">
+  <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492M5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0"/>
+  <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115z"/>
+</svg>
+
+
+
+
+
       </div>
       
 
